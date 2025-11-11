4dd4fadcfe3f971b08fda13f4b86f0bd2a561f5b
lucaslng <saculgnuel1@gmail.com>
2025-11-11 12:36:52 -0500

    quiet dotenv

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const readline = require("readline");
-require('dotenv').config();
+require('dotenv').config({quiet: true});
 
 const app = express();
 const PORT = 6767;
