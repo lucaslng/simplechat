deea67c31be61d7edc7442945415510de55854bf
lucaslng <saculgnuel1@gmail.com>
2025-11-14 13:25:05 -0500

    mdns test

---
+++ const.js
@@ -0,0 +1,6 @@
+// const.js
+
+const SERVICE_TYPE = "37e69f0f-2054-4d8c-a589-76fe73552fcf";
+const PORT = 55567;
+
+export { SERVICE_TYPE, PORT };
--- mdns.js
+++ mdns.js
@@ -1,11 +1,12 @@
 // mdns.js
 
 import { Bonjour } from 'bonjour-service';
+import { SERVICE_TYPE, PORT } from './const.js';
 
 const instance = new Bonjour();
 
-instance.publish({ name: 'Wassup server', type: 'wassup', port: 55567 })
+instance.publish({ name: ' ', host: ' ', type: SERVICE_TYPE, port: PORT });
 
-instance.find({ type: 'wassup' }, function (service) {
+instance.find({ type: SERVICE_TYPE }, function (service) {
   console.log('Found a server:', service)
 })
