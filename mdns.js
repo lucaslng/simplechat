bb86496654291b3025ba075791f53f18f65be53c
Ryan Cheung <ryanteddy1716@gmail.com>
2026-01-31 11:53:13 -0500

    potential fixes

--- mdns.js
+++ mdns.js
@@ -13,33 +13,47 @@ const mdns = mDNS({ loopback: false });
 export const servers = new Set();
 
 mdns.on("response", function (response) {
-  pr(response.answers);
-  response.answers
-    .filter((answer) => answer.name === SERVICE_NAME && net.isIPv4(answer.data.toString()))
-    .forEach((answer) => {
-      pr("Found server:", answer.data.toString());
-      servers.add(answer.data.toString());
-    });
+	response.answers
+		.filter((answer) => answer.name === SERVICE_NAME && answer.type === "A")
+		.forEach((answer) => {
+		const ip = answer.data;
+		if (net.isIPv4(ip) && ip !== getLocalIP()) {
+			pr("Found server:", ip);
+			servers.add(ip);
+		}
+	});
 });
 
 mdns.on("query", function (query) {
-  pr(query.questions);
-  if (query.questions[0] && query.questions[0].name === SERVICE_NAME) {
-    mdns.respond([
-      {
-        name: SERVICE_NAME,
-        type: "TXT",
-        data: getLocalIP(),
-      },
-    ]);
-  }
+	if (query.questions.some((q) => q.name === SERVICE_NAME)) {
+		mdns.respond([
+		{
+			name: SERVICE_NAME,
+			type: "A",
+			ttl: 300,
+			data: getLocalIP(),
+		},
+		]);
+	}
 });
 
+
 mdns.query({
-  questions: [
-    {
-      name: "hello",
-      type: "A",
-    },
-  ],
+	questions: [
+		{
+		name: SERVICE_NAME,
+		type: "A",
+		},
+	],
 });
+
+setInterval(() => {
+	mdns.query({
+		questions: [
+		{
+			name: SERVICE_NAME,
+			type: "A",
+		},
+		],
+	});
+}, 30000);
--- server.js
+++ server.js
@@ -3,41 +3,41 @@
 import { NAME, PORT } from "./const.js";
 import { servers } from "./mdns.js";
 import { rl, pr } from "./inout.js";
+import sendMsg from "./sendMsg.js";
 
 import express, { json, urlencoded } from "express";
 
 const app = express();
 console.log("Hello", NAME);
 
-
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
-  pr(`${req.body.name} on ${req.ip.split(':').at(-1)}: ${req.body.message}`);
-  res.status(200);
+	pr(`${req.body.name} on ${req.ip.split(":").at(-1)}: ${req.body.message}`);
+	res.status(200).send();
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-  rl.prompt();
+	console.log("Server running on port", PORT);
+	rl.prompt();
 });
 
 rl.on("line", async (line) => {
-  const input = line.trim();
-  if (input) {
+	const input = line.trim();
+	if (input) {
     servers.forEach(async (server) => {
-      try {
-        const response = sendMsg(server, input);
-        if (!response.ok) {
-          pr("Response", response.status, "for", server);
-          servers.delete(server);
-        }
-      } catch (error) {
-        pr("Error sending message to", server);
-        servers.delete(server);
-      }
-    });
-  }
-  rl.prompt();
+		try {
+			const response = await sendMsg(server, input);
+			if (!response.ok) {
+			pr("Response", response.status, "for", server);
+			servers.delete(server);
+			}
+		} catch (error) {
+			pr("Error sending message to", server);
+			servers.delete(server);
+		}
+		});
+	}
+	rl.prompt();
 });
